8cfd8a653ffc55ba093444c69bafa7868fcf15b0
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-30 17:25:26 +0200

    little fix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main() {
     int* a = new int;
     cin >> *a;
     cout << (*a == 0 ? "zero" :(*a < 0 ? "negative" : "positive")) << endl;
-    delete a;
+    if (a != nullptr)
+    {
+        delete a;
+    }
     return 0;
 }
