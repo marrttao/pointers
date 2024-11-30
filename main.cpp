a892e4e35491c04a9dfeb86f60156a33df3c5d2b
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-30 17:21:11 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+	int* a = new int;
+	int* b = new int;
+	cin >> *a;
+	cin >> *b;
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+	cout << *a << "\t" << *b << endl;
+	delete a, b;
 	return 0;
 }
