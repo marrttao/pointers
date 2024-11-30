699fe85fb045812a4507c224c3ae840b4acaaaff
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-30 17:39:32 +0200

    completed task 5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+int main()
+{
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Array elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    cout << "Sum of array elements: " << sum << endl;
+
+	if (arr != nullptr)
+		delete[] arr;
+    return 0;
 }
