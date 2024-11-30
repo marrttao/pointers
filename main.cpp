dfa1af125ff17ea985106d53e99e5ed5db92a119
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-30 17:12:15 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-	return 0;
+    int* a = new int;
+    int* b = new int;
+
+    cout << "Enter value for a: ";
+    cin >> *a;
+    cout << "Enter value for b: ";
+    cin >> *b;
+
+    cout << (*a > *b ? *a : *b) << endl;
+
+    delete a;
+    delete b;
+    return 0;
 }
