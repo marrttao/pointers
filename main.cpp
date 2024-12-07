f0b45320c324e5f87004ae9c50842e5027569dca
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-07 15:35:08 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 int main() {
-	return 0;
+    int size;
+    cin >> size;
+    int* array = new int[size] {};
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+    int* temp = new int[size] {};
+    for (int i = 0; i < size; i++) {
+        array[i] = dis(gen);
+    }
+    cout << "First array: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    
+    for (int i = 0; i < size; i++) {
+        temp[i] = array[size - 1 - i];
+    }
+    int* second_array = new int[size] {};
+    
+    for (int i = 0; i < size; i++) {
+        second_array[i] = temp[i];
+    }
+    cout << endl << "Second array where first is reversed: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << second_array[i] << " ";
+    }
+
+    delete[] array;
+    delete[] temp;
+    delete[] second_array;
+
+    return 0;
 }
